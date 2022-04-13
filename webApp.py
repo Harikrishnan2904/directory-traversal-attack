9693eb7f4ee089157d805d5e913219b1ad9b4e22
hari <harikrishnan29042001@gmail.com>
2022-04-13 11:12:58 +0530

    vulnerable server ready

--- webApp.py
+++ webApp.py
@@ -10,11 +10,25 @@ home_page = """
     <body>
     <br />
     <form action="/file" method="GET">
-      <h1>Files</h1>
-      <div class="form-group">
-        <label for="file1">file name</label>
-        <input class="form-control" type="text" name="file" placeholder="Enter file name">
-      </div>
+        <h1>Files</h1>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="file" id="exampleRadios1" value="file1.txt" checked>
+        <label class="form-check-label" for="exampleRadios1">
+            File 1
+        </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="file" id="exampleRadios2" value="file2.txt">
+        <label class="form-check-label" for="exampleRadios2">
+            File 2
+        </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="file" id="exampleRadios2" value="file3.txt">
+        <label class="form-check-label" for="exampleRadios2">
+            File 3
+        </label>
+        </div>
       <button type="submit" class="btn btn-success">Open</button>
     </form>
     </body>
@@ -26,8 +40,8 @@ def home():
     return home_page
 
 @app.route('/file')
-def comments():   
-    filename = "Z:/sem6/security in computing/directoryTraversalAttack/files/"+request.args.get('file')
+def comments():
+    filename = "C:/Users/Rohith Ram/Desktop/directory-traversal-attack/files/"+request.args.get('file')
     #print(filename)
     file = open(filename,'r')
     output = file.read()
@@ -37,3 +51,23 @@ def comments():
 if __name__=="__main__":
     app.run(debug=False)
 
+"""
+"../../../../../windows\system32\license.rtf"
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title> Home</title>
+    </head>
+    <body>
+    <br />
+    <form action="/file" method="GET">
+      <h1>Files</h1>
+      <div class="form-group">
+        <label for="file1">file name</label>
+        <input class="form-control" type="text" name="file" placeholder="Enter file name">
+      </div>
+      <button type="submit" class="btn btn-success">Open</button>
+    </form>
+    </body>
+</html>
+"""
